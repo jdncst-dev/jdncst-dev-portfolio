3b1f28b01a7c944dee5c207ba2d800a589473d0b
Jordan Castiglioni <jordan@jdncst.dev>
2026-05-14 00:38:35 +0200

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { ThemeProvider } from '@/components/theme-provider'
+import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 const geistSans = Geist({
@@ -84,6 +85,7 @@ export default function RootLayout({
         <ThemeProvider attribute='data-theme' defaultTheme='light' enableSystem>
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
